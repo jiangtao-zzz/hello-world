625935d904df31dab3138a03751b5f767d47aefa
york <17695958740@163.com>
2025-10-11 15:39:47 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    放松放松 how it going
+    this is test branch
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
